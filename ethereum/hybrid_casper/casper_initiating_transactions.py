0f46125208726dad4695caeaf150642a50ca0866
Karl Floersch <karl@karlfloersch.com>
2017-12-30 23:55:06 +0700

    Update initializing transactions

--- ethereum/hybrid_casper/casper_initiating_transactions.py
+++ ethereum/hybrid_casper/casper_initiating_transactions.py
@@ -34,7 +34,8 @@ def mk_initializers(config, sender_privkey, starting_nonce=0):
         nonce += 1
     # Casper initialization transaction
     params = [config["EPOCH_LENGTH"], config["WITHDRAWAL_DELAY"], config["OWNER"], sig_hasher_address,
-              purity_checker_address, config["BASE_INTEREST_FACTOR"], config["BASE_PENALTY_FACTOR"]]
+              purity_checker_address, config["BASE_INTEREST_FACTOR"], config["BASE_PENALTY_FACTOR"],
+              config["MIN_DEPOSIT_SIZE"]]
     # Casper deploy bytecode
     casper_deploy_bytecode = casper_bytecode + casper_ct.encode_constructor_arguments(params)
     casper_deploy_tx = Transaction(nonce, gasprice, 5000000, b'', 0, casper_deploy_bytecode).sign(sender_privkey)
