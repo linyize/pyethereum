279ba4d25842a0d3b0c046dc209079f48951b917
lyz <linyize@gmail.com>
2018-04-11 21:37:34 +0800

    IMO分叉高度

--- ethereum/hybrid_casper/config.py
+++ ethereum/hybrid_casper/config.py
@@ -15,12 +15,12 @@ casper_config = dict(
     MIN_DEPOSIT_SIZE=1500 * 10**18,
 
     # IMO fork
-    IMO_FORK_BLKNUM=400, # 100个块以后开始要求特殊的共识规则 
+    IMO_FORK_BLKNUM=10000, # 100个块以后开始要求特殊的共识规则 
     IMO_FORK_MIN_MINER_BALANCE=1000, # coinbase必须有大于1000eth
     IMO_FORK_BLK_NOTREPEAT=10, # 10个块内不得出现重复的coinbase
-    HOMESTEAD_FORK_BLKNUM=0,
-    SPURIOUS_DRAGON_FORK_BLKNUM=0,
-    DAO_FORK_BLKNUM=0,
+    HOMESTEAD_FORK_BLKNUM = 2**99,
+    SPURIOUS_DRAGON_FORK_BLKNUM = 2**99,
+    DAO_FORK_BLKNUM = 2**99,
 )
 
 config.casper_config = {**config.default_config, **casper_config}
