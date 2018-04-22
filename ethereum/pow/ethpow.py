875481860aaff25eefac79e56700918c903119ff
lyz <linyize@gmail.com>
2018-04-22 22:19:39 +0800

    降低出块频率

--- ethereum/pow/ethpow.py
+++ ethereum/pow/ethpow.py
@@ -156,7 +156,7 @@ def mine(block_number, difficulty, mining_hash, start_nonce=0, rounds=1000):
     return None, None
 
 def mine_lowcost(block_number, difficulty, mining_hash, start_nonce=0, rounds=1000):
-    if random.randint(1, 5) != 3: # 1-10中随机到5才出块
+    if random.randint(1, 15) != 10: # 1-10中随机到5才出块
         return None, None
 
     assert utils.is_numeric(start_nonce)
